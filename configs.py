5b3f4c706cca8f9ea40b453e5f46dafb5d9bec57
ViSHWA <154061854+ViSHWA14510@users.noreply.github.com>
2024-04-28 02:44:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,9 +19,9 @@ class Config(object):
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
-This is Permanent Files Store Bot!
+This is Permanent Files Store Bot! 🗃️
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
-**Any Issue Msg Me - @iTS_ViSHWA14**
+**Any Issue Msg Me - @iTS_ViSHWA14** 😎
 
 🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
 
@@ -47,9 +47,9 @@ Also remember that developer will Delete Adult Contents from Database. So better
 [Donate Now](https://www.paypal.me/) (PayPal)
 """
 	HOME_TEXT = """
-Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
+Hi, 👋 [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot** 🗃️.
 
-Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
+Send me any file I will give you a permanent Sharable Link 🔗. I Support Channel Also! Check **About Bot 🤖** Button.
 
-**If Do You Want Any Marathi Serial Channel Link MSG Me Here - @iTS_ViSHWA14**
+**If Do You Want Any 'Marathi Serial Channel Link' MSG Me Here - @iTS_ViSHWA14**
 """
