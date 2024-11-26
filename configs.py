b9dccd903a265c51ad12ef3944cee35a00f61378
ViSHWA <154061854+ViSHWA14510@users.noreply.github.com>
2024-11-27 00:02:43 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,19 +4,19 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "27344784"))
-	API_HASH = os.environ.get("API_HASH", "a12613f54c74d00bfb76cf8f0688c2ee")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6923368496:AAFhmdZNztJWF-0PLwW3dBtl4tvAZUxm_70")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "Marathi_Serials_Provider_Bot")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002056408106"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "6899191648"))
-	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Drxfile:Drxfile@drxfile.teugktb.mongodb.net/?retryWrites=true&w=majority")
-	UPDATES_CHANNEL = int(os.environ.get("UPDATES_CHANNEL", "-1002091923966"))
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002048118352"))
-	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
+	API_ID = int(os.environ.get("API_ID", ""))
+	API_HASH = os.environ.get("API_HASH", "")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "")
+	UPDATES_CHANNEL = int(os.environ.get("UPDATES_CHANNEL", ""))
+	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
 This is Permanent Files Store Bot! 🗃️
